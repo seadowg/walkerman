a251b43cfb19688b72e464eb4c472d2ba63ecca0
Callum Stott <callum@seadowg.com>
2015-03-13 22:09:18 +0000

    Add templating

--- src/main/kotlin/application.kt
+++ src/main/kotlin/application.kt
@@ -2,12 +2,21 @@ package com.seadowg.walkerman
 
 import spark.Spark.*
 import spark.SparkBase.port
+import com.github.mustachejava.DefaultMustacheFactory
+import java.io.StringWriter
 
 public fun main(args: Array<String>) {
   val portString = System.getenv().get("PORT") ?: "9000";
   port(Integer.parseInt(portString))
 
-  get("/", { req, res -> "Hello Walkerman!" })
+  get("/", { req, res ->
+    val mustacheFactory = DefaultMustacheFactory()
+    val mustache = mustacheFactory.compile("index.mustache")
+    val stringWriter = StringWriter()
+
+    mustache.execute(stringWriter, null).close();
+    stringWriter.toString()
+  })
 }
 
 

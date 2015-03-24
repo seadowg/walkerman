825898b0691c3e1a5e442f8f4a16a8b811a3b136
Callum Stott <callum@seadowg.com>
2015-03-24 22:06:31 +0000

    Grab postgres URI from VCAP in CF

--- src/main/kotlin/com/seadowg/walkerman/database/database.kt
+++ src/main/kotlin/com/seadowg/walkerman/database/database.kt
@@ -1,10 +1,21 @@
 package com.seadowg.walkerman.database
 
 import org.postgresql.ds.PGPoolingDataSource
+import org.json.JSONObject
 
 val dataSource = PGPoolingDataSource()
 
 fun configure(): Unit {
-    dataSource.setUrl("jdbc:postgresql://localhost:5432/walkerman")
+    val env = System.getenv()
+
+    if (env.containsKey("VCAP_SERVICES")) {
+        val vcapServices = JSONObject(env.get("VCAP_SERVICES"))
+        val postgres = vcapServices.getJSONArray("elephantsql")?.getJSONObject(0)
+        val uri = postgres?.getJSONObject("credentials")?.getString("uri")
+
+        if (uri != null) dataSource.setUrl(uri)
+    } else {
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/walkerman")
+    }
 }
 

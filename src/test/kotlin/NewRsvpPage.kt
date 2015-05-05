b9861bea5203fa49472ddb037fc1989cb7d00a74
Callum Stott <callum@seadowg.com>
2015-05-05 16:19:10 -0700

    Use page objects for new rsvp page tests

---
+++ src/test/kotlin/NewRsvpPage.kt
@@ -0,0 +1,26 @@
+
+import org.fluentlenium.core.FluentPage
+
+class NewRsvpPage : FluentPage() {
+    public override fun getUrl(): String {
+        return "http://localhost:9000/rsvps/new"
+    }
+
+    public fun fillInNameAndEmail(email: String, name: String) {
+        fill("#rsvp_email").with(email)
+        fill("#rsvp_name").with(name)
+    }
+
+    public fun submit() {
+        submit("#rsvp_submit")
+    }
+
+    public fun addExtraGuests(guests: Int) {
+        find("#rsvp_guests_yes").click()
+        fill("#rsvp_guests").with(guests.toString())
+    }
+
+    public fun removeExtraGuests() {
+        find("#rsvp_guests_no").click()
+    }
+}
